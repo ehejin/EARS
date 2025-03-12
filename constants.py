2ea2aa1d25dfb46f4341e116ea7578c608a41250
Alice Chen <alicezychen@gmail.com>
2025-03-12 00:10:41 -0700

    edit prompt

--- constants.py
+++ constants.py
@@ -33,8 +33,8 @@ CLEANED_QUIZ_ANSWERS_PROMPT = "Here are the answers to a 5 question quiz:\n\n {0
                                the list are the exact same as in the provided answer."
 
 GET_QUESTION_PROMPT = "Retrieve the {0}th question from the following quiz. ONLY include the question and answer choices in \
-                       your response and do not include the answer or any other information before or \
-                       after the question. Here is the quiz:\n\n{1}"
+                       your response and do not include the answer. Make sure the questions are in a properly formatted bulleted \
+                       Here is the quiz:\n\n{1}"
 
 # Responses from our bt 
 INTERACTIVE_QUIZ_FINISH = "🎉Congrats on finishing an interactive quiz session!🎉\n \
